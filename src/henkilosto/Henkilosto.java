d675eb3111a7a67617551cdebb2443176ec22488
annikaheino <32327491+annikaheino@users.noreply.github.com>
2019-02-23 23:08:33 +0200

    Commit 3

--- src/henkilosto/Henkilosto.java
+++ src/henkilosto/Henkilosto.java
@@ -16,6 +16,7 @@ public class Henkilosto {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Branch test.");
     }
     
 }
