edb0da5fb8097dfd0234ff3f1c8f02464d17995d
annikaheino <32327491+annikaheino@users.noreply.github.com>
2019-03-10 22:15:23 +0200

    Viikko 10, Versio 0.1, Tekstikäyttöliittymä

---
+++ src/personnel/PersonnelMain.java
@@ -0,0 +1,18 @@
+package personnel;
+/**
+ *
+ * @author Annika Heino
+ */
+
+public class PersonnelMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        PersonnelUI personnelUI = new PersonnelUI();
+        personnelUI.startApplication();
+
+    }
+}
